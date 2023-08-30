6fb6de6e33112ec0c805245219ad3552294bfa5f
Leandro <pupif09@gmail.com>
2023-08-29 21:01:10 -0300

    29.08.23 - Iniciando Proveedores.

--- frmAyuda.Designer.cs
+++ frmAyuda.Designer.cs
@@ -29,10 +29,19 @@ namespace pryFernandezIES
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // frmAyuda
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "frmAyuda";
+            this.ClientSize = new System.Drawing.Size(631, 405);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "frmAyuda";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Ayuda";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- frmClientes.Designer.cs
+++ frmClientes.Designer.cs
@@ -29,10 +29,19 @@ namespace pryFernandezIES
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // frmClientes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "frmClientes";
+            this.ClientSize = new System.Drawing.Size(631, 405);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "frmClientes";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Clientes";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
